f843ec2aba0c3502b031a2b107e7f14f383016f0
Onwuka Stanley <kachi2505@yahoo.com>
2019-12-16 16:13:33 +0000

    Registration page

---
+++ dist/scripts/register_app.js
@@ -0,0 +1,46 @@
+const siblingsData = document.querySelector('.siblings');
+const numberOfSiblings = document.getElementById('number-of-siblings');
+const yesRadio = document.getElementById('yes_radio');
+const noRadio = document.getElementById('no_radio');
+const defectsList = document.getElementById('defects_list');
+const siblingInfoString = `<div class="siblings-data">
+    <h3 class="title__subtitle">Siblings Data</h3>
+    <hr class="underline">
+    <div class="siblings-input">
+        <label for="s_name">Full Name:</label>
+        <input type="text" name="S_name" id="S_name">
+    </div>
+    <div class="siblings-input">
+        <label for="s_class">Class:</label>
+        <input type="text" name="S_class" id="S_class">
+    </div>
+    <div class="siblings-input">
+        <label for="s_year">Year:</label>
+        <input type="text" name="S_year" id="S_year">
+    </div>
+    <hr>
+    </div>`;
+
+//Event Listeners
+yesRadio.addEventListener('click', () => {
+    defectsList.style.display = 'block';
+});
+
+noRadio.addEventListener('click', () => {
+    defectsList.style.display = 'none';
+});
+
+
+
+numberOfSiblings.addEventListener('change', () => {
+    if(siblingsData.childElementCount > numberOfSiblings.value){
+        siblingsData.innerHTML = '';
+    }
+    const numberOfForms = numberOfSiblings.value - siblingsData.childElementCount;
+    for(let i=0; i < numberOfForms; i++){
+        const siblingInfoEl = new DOMParser().parseFromString(siblingInfoString, "text/html").firstChild;
+        siblingsData.appendChild(siblingInfoEl);
+    }
+});
+
+
