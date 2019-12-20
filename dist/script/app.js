2e0cba5e480c7bdcd1bcae931410355a2a2a8cbd
Onwuka Stanley <kachi2505@yahoo.com>
2019-12-20 18:32:21 +0000

    Needs a fix from Emmanuel

--- dist/script/app.js
+++ dist/script/app.js
@@ -1,20 +1,24 @@
 const menu = document.querySelector(".menu")
 const navHidden = document.querySelector(".nav-overlay")
 const back = document.querySelector(".backBtn")
-
-
+const hamburger = document.querySelector('.hidden-menu');
+const mobileNav = document.querySelector('.menubar');
 
 const menuSan = () => {
-    console.log("menuuuuu")
+    // console.log("menuuuuu")
     navHidden.classList.add("show")
 }
 
-menu.addEventListener("click", menuSan)
+menu.addEventListener("click", menuSan);
 
 
 const backSan = () => {
-    console.log("backkkk")
+    // console.log("backkkk")
     navHidden.classList.remove("show")
 }
+
+// hamburger.addEventListener('click', () => {
+
+// })
 
 back.addEventListener("click", backSan)
