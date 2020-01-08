397aa05383d4a627cebce87dbd38a359ee6df1ea
Curious <170408025@live.unilag.edu.ng>
2020-01-09 00:36:49 +0100

    fixed bug on about page

--- bammysite/static/script/about_app.js
+++ bammysite/static/script/about_app.js
@@ -34,3 +34,29 @@ $('.post-wrapper').slick({
         // instead of a settings object
       ]
   });
+
+
+const menu = document.querySelector(".menu")
+const navHidden = document.querySelector(".nav-overlay")
+const back = document.querySelector(".backBtn")
+const hamburger = document.querySelector('.hidden-menu');
+const mobileNav = document.querySelector('.menubar');
+
+const menuSan = () => {
+    // console.log("menuuuuu")
+    navHidden.classList.add("show")
+}
+
+menu.addEventListener("click", menuSan);
+
+
+const backSan = () => {
+    // console.log("backkkk")
+    navHidden.classList.remove("show")
+}
+
+// hamburger.addEventListener('click', () => {
+
+// })
+
+back.addEventListener("click", backSan)
